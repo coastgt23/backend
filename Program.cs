68dd5356b5197d2d4a4d450c9bf8fb342f30d151
coastgt23 <teddie250214@gmail.com>
2026-08-02 14:12:00 +0100

    Initial commccit

--- Program.cs
+++ Program.cs
@@ -4,7 +4,15 @@ namespace Stella
     {
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
+            var builder = WebApplication.CreateBuilder(new WebApplicationOptions
+            {
+                Args = args
+            });
+
+            builder.Configuration.Sources
+                .OfType<Microsoft.Extensions.Configuration.Json.JsonConfigurationSource>()
+                .ToList()
+                .ForEach(x => x.ReloadOnChange = false);
 
             builder.Services.AddAuthentication();
             builder.Services.AddAuthorization();
